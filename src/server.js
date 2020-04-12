989be918599337954f75c456b00cf713eef6b6ed
Anton Shchokotov <58229945+antonshchokotov@users.noreply.github.com>
2020-04-12 10:57:12 +0300

    fix: put process error handlers before app initialization

--- src/server.js
+++ src/server.js
@@ -3,10 +3,6 @@ const app = require('./app');
 const logger = require('./common/logger');
 const exit = process.exit;
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
-
 process.on('uncaughtException', (error, origin) => {
   logger.log({
     timestamp: new Date().toISOString(),
@@ -29,3 +25,7 @@ process.on('unhandledRejection', (reason, promise) => {
   });
   exit(1);
 });
+
+app.listen(PORT, () =>
+  console.log(`App is running on http://localhost:${PORT}`)
+);
